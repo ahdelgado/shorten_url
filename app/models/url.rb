9c2a633f49a01a135c8d27ac02a6164d79d2d012
Alcides Ruiz <ahr@alumni.cmu.edu>
2018-03-25 13:31:18 -0400

    Make sure there are no hash output collisions in database

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,8 +2,7 @@ require 'digest'
 
 class Url < ActiveRecord::Base
     
-  VALID_URL_REGEX = /\A((https?):\/\/)?[a-z0-9]+([\-\.]
-                      {1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix
+  VALID_URL_REGEX = /\A((https?):\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix
 
   VALID_CHARS = ('a'..'z').to_a + (0..9).to_a + ('A'..'Z').to_a
                       
@@ -14,16 +13,14 @@ class Url < ActiveRecord::Base
 
   # Hash long URL into short URL
   def shorten_url
-    self.short_url = Digest::SHA256.base64digest(long_url)
+    (5..Digest::SHA256.base64digest(long_url).length).each do |i|
+      self.short_url = Digest::SHA256.base64digest(long_url).slice(0..i)
+      break if Url.find_by_short_url(self.short_url).nil?
+    end
   end
 
   def clean
     self.url = self.long_url.strip.downcase.gsub(/(https?:\/\/)|(www\.)/, '')
     self.url = "http://#{self.url}"
   end
-
-  def display_url
-    short_url.slice(0..5)
-  end
-            
 end
