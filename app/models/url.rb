5013d5a988250c79e37d78b1688e2d76e9ae1961
Alcides Ruiz <ahr@alumni.cmu.edu>
2018-03-26 16:45:00 -0400

    Force long URLs to fit in columns, fixed a bug which caused a forward slash to appear in short URL breaking redirection, and removed down-casing in clean method also breaking redirection
    Further constrain long URL length in column

--- app/models/url.rb
+++ app/models/url.rb
@@ -15,12 +15,21 @@ class Url < ActiveRecord::Base
   def shorten_url
     (5..Digest::SHA256.base64digest(long_url).length).each do |i|
       self.short_url = Digest::SHA256.base64digest(long_url).slice(0..i)
+      self.short_url.gsub!(/\//, 'a') if  self.short_url.include?('/')
       break if Url.find_by_short_url(self.short_url).nil?
     end
   end
 
   def clean
-    self.url = self.long_url.strip.downcase.gsub(/(https?:\/\/)|(www\.)/, '')
-    self.url = "http://#{self.url}"
+    self.url = self.long_url.strip.gsub(/(https?:\/\/)|(www\.)/, '')
+    self.url = "https://#{self.url}"
+  end
+
+  def display_url
+    if self.long_url.length > 80
+      self.long_url.slice(0..79).gsub!(/$/, '...')
+    else
+      self.long_url
+    end
   end
 end
